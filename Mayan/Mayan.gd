c70b19e606e512c6ea7aaf3736beec2626083eba
Martin Castel <martin.castel@hotmail.fr>
2020-11-14 13:13:01 +0100

    feature: mayans get angrier the further they are

--- Mayan/Mayan.gd
+++ Mayan/Mayan.gd
@@ -1,7 +1,12 @@
 extends RigidBody2D
 
 var initial_position := Vector2.ZERO;
+var collision_count = 0
+var angriness = 0.0
+var furthest_distance_ever = 0.0
 
+const ACCEPTABLE_DISTANCE = 10 # max distance before mayans get angry
+const MAX_DISTANCE = 50
 const FRICTION = 0.3
 const SPEED = 0.5
 
@@ -19,5 +24,20 @@ func _go_back_to_original_position_force(state):
 	var direction_towards_initial_position = position.direction_to(initial_position)
 	var distance_to_initial_position = position.distance_to(initial_position)
 	
-	if distance_to_initial_position > 0:
+	if distance_to_initial_position > furthest_distance_ever:
+		furthest_distance_ever = distance_to_initial_position
+	
+	#if distance_to_initial_position < ACCEPTABLE_DISTANCE:
+	#	angriness = 0.0
+	#else:
+		#angriness = max(0.0, min(1.0, (furthest_distance_ever*1.0 - ACCEPTABLE_DISTANCE*1.0) / (MAX_DISTANCE*1.0 - ACCEPTABLE_DISTANCE*1.0)))
+	angriness = max(0.0, min(1.0, 1.0*(furthest_distance_ever - ACCEPTABLE_DISTANCE) / (MAX_DISTANCE - ACCEPTABLE_DISTANCE)))
+	self.modulate.g = 1.0 - angriness
+	self.modulate.b = 1.0 - angriness
+	
+	
+	if distance_to_initial_position > ACCEPTABLE_DISTANCE:
 		state.apply_central_impulse(direction_towards_initial_position * distance_to_initial_position * SPEED)
+
+		
+		
