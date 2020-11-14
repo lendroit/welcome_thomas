777c5f8255f3478e80003485f751b71043f9afe9
Martin Castel <martin.castel@hotmail.fr>
2020-11-14 14:13:44 +0100

    feature: they turn red when angry

--- Mayan/Mayan.gd
+++ Mayan/Mayan.gd
@@ -2,10 +2,12 @@ extends RigidBody2D
 
 var initial_position := Vector2.ZERO;
 var collision_count = 0
-var angriness = 0.0
+var angriness = 0.0 # how mad they are, from 0.0 to 2.0
 var furthest_distance_ever = 0.0
+var distance_to_initial_position = 0.0
 
-const ACCEPTABLE_DISTANCE = 10 # max distance before mayans get angry
+const ACCEPTABLE_DISTANCE = 0 # max distance before mayans get angry
+const MAX_ANGRINESS_BEFORE_ITS_TOO_LATE = 1.5
 const MAX_DISTANCE = 50
 const FRICTION = 0.3
 const SPEED = 0.5
@@ -14,30 +16,37 @@ func _ready():
 	initial_position = self.position
 
 func _integrate_forces(state):
+	_get_distance_to_initial_position(state)
 	_friction_force(state)
 	_go_back_to_original_position_force(state)
+	_get_angriness(state)
+	_turn_color(state)
+	
+func _get_distance_to_initial_position(state):
+	distance_to_initial_position = position.distance_to(initial_position)
+	if distance_to_initial_position > furthest_distance_ever:
+		furthest_distance_ever = distance_to_initial_position
+	
+func _get_angriness(state):
+	angriness = max(0.0, min(2.0, 1.0*(furthest_distance_ever - ACCEPTABLE_DISTANCE) / (MAX_DISTANCE - ACCEPTABLE_DISTANCE)))
+	#if distance_to_initial_position < ACCEPTABLE_DISTANCE*2.0:
+	if angriness <1.5:
+		furthest_distance_ever *= 0.99
+		
+func _turn_color(state):
+	self.modulate.g = 1.0 - min(1.0, angriness)
+	self.modulate.b = 1.0 - min(1.0, angriness)
+	self.modulate.r = min(1.0, 2.0 - angriness)
+	
 	
 func _friction_force(state):
 	state.apply_central_impulse(-state.linear_velocity * FRICTION)
 
 func _go_back_to_original_position_force(state):
 	var direction_towards_initial_position = position.direction_to(initial_position)
-	var distance_to_initial_position = position.distance_to(initial_position)
 	
-	if distance_to_initial_position > furthest_distance_ever:
-		furthest_distance_ever = distance_to_initial_position
-	
-	#if distance_to_initial_position < ACCEPTABLE_DISTANCE:
-	#	angriness = 0.0
-	#else:
-		#angriness = max(0.0, min(1.0, (furthest_distance_ever*1.0 - ACCEPTABLE_DISTANCE*1.0) / (MAX_DISTANCE*1.0 - ACCEPTABLE_DISTANCE*1.0)))
-	angriness = max(0.0, min(1.0, 1.0*(furthest_distance_ever - ACCEPTABLE_DISTANCE) / (MAX_DISTANCE - ACCEPTABLE_DISTANCE)))
-	self.modulate.g = 1.0 - angriness
-	self.modulate.b = 1.0 - angriness
-	
-	
-	if distance_to_initial_position > ACCEPTABLE_DISTANCE:
-		state.apply_central_impulse(direction_towards_initial_position * distance_to_initial_position * SPEED)
+	if distance_to_initial_position > ACCEPTABLE_DISTANCE/2.0:
+		state.apply_central_impulse(direction_towards_initial_position * distance_to_initial_position * min(1.0, angriness))
 
-		
-		
+	else:
+		return null
