f7491e81f55987f892b8d761005d7da479ffd9e8
Sammy Teillet <sammy.teillet@gmail.com>
2020-11-14 22:28:03 +0100

    feature: Player rotate and dies

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,6 +8,7 @@ const FRICTION = 10000
 
 var velocity = Vector2.ZERO
 var item_held = null
+var is_dead = false
 
 onready var reachableObjectsArea = $ReachableObjectsArea
 onready var animation_player = $AnimationPlayer
@@ -29,13 +30,15 @@ func has_entered_drop_area():
 		emit_signal("player_won")
 
 func has_been_hit(body):
-	print("Oh je suis touchey")
 	_die()
 
 func _die():
-	queue_free()
+	is_dead = true
+	self.rotation = PI/4
 
 func _input(event):
+	if is_dead:
+		return
 	if event.is_action_pressed("ui_accept"):
 		if !item_held:
 			var reachable_item = reachableObjectsArea.get_item_if_any()
@@ -45,7 +48,9 @@ func _input(event):
 			_drop_item()
 
 func _physics_process(delta):
-	
+	if is_dead:
+		return
+
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
