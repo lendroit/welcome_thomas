d143c87534c6a38a754e65a80c78d694a12f8ac8
Pierre Poupin <pierrep@bam.tech>
2020-11-14 03:09:32 +0100

    feature: add mayan going back to their place

--- Mayan/Mayan.gd
+++ Mayan/Mayan.gd
@@ -1,21 +1,16 @@
 extends RigidBody2D
 
+var initial_position := Vector2.ZERO;
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const FRICTION = 0.3
+const SPEED = 0.5
 
-
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	initial_position = self.position
 
 func _integrate_forces(state):
-	state.apply_central_impulse(-state.linear_velocity * 0.3)
+	state.apply_central_impulse(-state.linear_velocity * FRICTION)
+
+	var direction_towards_initial_position = position.direction_to(initial_position)
+	var distance_to_initial_position = position.distance_to(initial_position)
+	state.apply_central_impulse(direction_towards_initial_position * distance_to_initial_position * SPEED)
