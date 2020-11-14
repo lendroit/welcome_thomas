cec2413b2a07c53234be4ed800c12af06eff92b4
Martin Castel <martin.castel@hotmail.fr>
2020-11-14 21:54:29 +0100

    remove some useless lines

--- World.gd
+++ World.gd
@@ -9,20 +9,14 @@ onready var replayButton = $Gui/PlayerWonLabel/Button
 var number_of_angry_mayans = 0
 var number_of_guards = 0
 
-signal guard_is_trigered
-
 func _on_new_angry_mayan():
 	number_of_angry_mayans += 1
-	print(number_of_angry_mayans)
 	if number_of_angry_mayans%5 == 0:
 		for child in mayans.get_children():
 			if (child.isGuard) and (child.angriness < child.MAX_ANGRINESS_BEFORE_ITS_TOO_LATE):
 				child.angriness = 2.0
 				child.furthest_distance_ever = 50000
 				break
-				
-		emit_signal("guard_is_trigered")
-		print("guard pas content")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
