0250d51b02af0574f20eeb6abeabd01f8cae4647
Sammy Teillet <sammy.teillet@gmail.com>
2020-11-14 11:36:38 +0100

    refactor: Extract force methods

--- Mayan/Mayan.gd
+++ Mayan/Mayan.gd
@@ -9,8 +9,13 @@ func _ready():
 	initial_position = self.position
 
 func _integrate_forces(state):
-	state.apply_central_impulse(-state.linear_velocity * FRICTION)
+	_friction_force(state)
+	_go_back_to_original_position_force(state)
+	
+func _friction_force(state):
+	state.apply_central_impulse(-state.linear_velocity *state.linear_velocity* FRICTION)
 
+func _go_back_to_original_position_force(state):
 	var direction_towards_initial_position = position.direction_to(initial_position)
 	var distance_to_initial_position = position.distance_to(initial_position)
 	
