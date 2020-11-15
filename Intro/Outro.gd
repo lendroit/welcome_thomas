d269033ddaa1794a2543ed91129bf1e750216ec7
Sammy Teillet <sammy.teillet@gmail.com>
2020-11-15 14:44:11 +0100

    feature: Restart at end of game

--- Intro/Outro.gd
+++ Intro/Outro.gd
@@ -1,7 +1,13 @@
 extends Control
 
-var World = preload("res://World.tscn")
+
+
+onready var end_of_game_music = $EndOfGameMusic
+
+func _ready():
+	end_of_game_music.play()
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
+		var World = load("res://World.tscn")
 		get_tree().change_scene_to(World)
