83d1a622d26dc75b21ef305efec9a4fd85cfa3f0
Pierre Poupin <pierrep@bam.tech>
2020-12-13 15:25:18 +0100

    fix mobile intro

--- Intro/Intro.gd
+++ Intro/Intro.gd
@@ -3,5 +3,5 @@ extends Control
 var World = preload("res://World.tscn")
 
 func _input(event):
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("ui_accept") || event is InputEventMouseButton:
 		get_tree().change_scene_to(World)
